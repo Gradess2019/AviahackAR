7d206edc430e8b41088489b00f5728723025b6ab
Stepan <trofimovjava@gmail.com>
2019-04-06 21:05:58 +0300

    Add Parser class

---
+++ Assets/Scripts/Parser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parser
+{
+    public Command ParseToCommand(string commandName, params string[] constructorArgs)
+    {
+        if (constructorArgs.Length > 0)
+        {   
+            if (commandName.Equals("TurnLightAction"))
+            {
+                return new TurnLightAction(constructorArgs);
+            } else if (commandName.Equals("HatchAction"))
+            {
+                return new HatchAction(constructorArgs);
+            }
+        }   
+        
+        return null;
+    }
+}
