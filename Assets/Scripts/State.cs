9acc365bc2c3994d6e1332592115aa480d76540a
Egor <eprf@mail.ru>
2019-04-06 21:54:15 +0300

    Added States

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : MonoBehaviour
+{
+    [SerializeField]
+    protected Material stateMaterial;
+
+    public void SetStateMaterial(Renderer renderer)
+    {
+        renderer.material = stateMaterial;
+    }
+}
